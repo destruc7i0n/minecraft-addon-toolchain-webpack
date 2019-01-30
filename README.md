23f9b202ff82e270eda71f2f2cdd341b044a68a6
destruc7i0n <dscdsouza@outlook.com>
2019-01-29 20:20:59 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,72 @@
+# minecraft-addon-toolchain-webpack
+
+## Webpack
+Adds support for modern JavaScript features not supported by the Minecraft Scripting Engine, such as modules and multi-file projects.
+
+This was heavily based on [minecraft-addon-toolchain-browserify](https://github.com/minecraft-addon-tools/minecraft-addon-toolchain/tree/master/packages/minecraft-addon-toolchain-browserify), but was adapted to use webpack.
+
+### Installing
+```powershell
+npm install --save-dev minecraft-addon-toolchain-webpack
+```
+
+### Adding to the toolchain
+```javascript
+const MinecraftAddonBuilder = require('minecraft-addon-toolchain/v1')
+const WebpackSupport = require('minecraft-addon-toolchain-webpack')
+
+const builder = new MinecraftAddonBuilder(<youraddonname>)
+builder.addPlugin(new WebpackSupport())
+
+module.exports = builder.configureEverythingForMe();
+```
+
+### Usage
+You can view an example of how this can be used on the original package's [README](https://github.com/minecraft-addon-tools/minecraft-addon-toolchain/tree/master/packages/minecraft-addon-toolchain-browserify#usage).
+
+### Configuration
+You can override the settings passed to Babelify any of the following settings the BrowserifySupport object.
+
+```javascript
+const MinecraftAddonBuilder = require('minecraft-addon-toolchain/v1')
+const WebpackSupport = require('minecraft-addon-toolchain-webpack')
+
+const builder = new MinecraftAddonBuilder(<youraddonname>);
+const webpackSupport = new WebpackSupport();
+
+/// Modify options that webpack will use
+//webpackSupport.webpackOptions
+
+/// Enable minification and other enhancements
+//webpackSupport.webpackOptions.mode = 'production'
+
+/// Modify the rules that webpack will use
+// IMPORTANT: Before doing so, check note below.
+//webpackSupport.webpackRules
+
+///Enable single file entry (more details below)
+//webpackSupport.options.multiEntry = false
+
+/// Change the intermediate output directory
+//webpackSupport.intermediateDir = './out/before-webpack'
+
+/// Change the entry point scripts that will be bundled.
+//webpackSupport.entryPoints = ['./scripts/client/*.js', './scripts/server/*.js']
+
+builder.addPlugin(webpackSupport)
+
+module.exports = builder.configureEverythingForMe();
+```
+
+#### *Multiple Entry Files
+If this is enabled (see info above) the compiler will only look for the files of `./scripts/client/client.js` and `./scripts/server/server.js`. This is useful if you want to only have one file as the output. By default this is disabled and will work exactly like [minecraft-addon-toolchain-browserify](https://github.com/minecraft-addon-tools/minecraft-addon-toolchain/tree/master/packages/minecraft-addon-toolchain-browserify).
+
+#### *Loaders
+To ensure that the package works out of the box, the loaders are loaded from the package's local `node_modules`. If you want to add your own loaders, set `options.resolveLoaders` to `false`, and install `babel-loader` (`npm i babel-loader`). Then proceed to edit the `webpackSupport.webpackRules` with your own loaders.
+
+
+For further information about the various configuration options provided by the various tools, consult the following links:
+
+| field name          | documentation link                                             |
+| ------------------- | -------------------------------------------------------------- |
+| `webpackOptions`    | https://webpack.js.org/configuration/                          |
---
+++ index.js
@@ -0,0 +1,147 @@
+const { series, dest } = require('gulp')
+
+const glob = require('glob')
+const tap = require('gulp-tap')
+const log = require('gulplog')
+const pump = require('pump')
+const path = require('path')
+const webpack = require('webpack-stream')
+
+class WebpackSupport {
+  constructor () {
+    this.options = {
+      debug: false,
+      multiEntry: true,
+      resolveLoaders: true
+    }
+    this.webpackOptions = {
+      mode: 'development' // for minification, set to `production`
+      // you can also add your own plugins, config etc here...
+    }
+    this.webpackRules = [ // modify/append this to add a file loader, etc.
+      {
+        test: /\.js$/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      }
+    ]
+
+    this.intermediateDir = './out/before-webpack'
+    this.entryPoints = ['./scripts/client/*.js', './scripts/server/*.js']
+
+    const _this = this
+    this.sourceTasks = [
+      {
+        condition: '**/*.js',
+        preventDefault: true,
+        task: (pack) => tap((file, t) => {
+          _this.options && _this.options.debug && log.info(`\twebpack: redirecting ${file.path}`)
+          return t.through(dest, [path.join(this.intermediateDir, pack.relativePath)])
+        })
+      }
+    ]
+  }
+
+  set builder (builder) {
+    if (builder._version < 1) {
+      throw new Error('webpack support requires using a minecraft-addon-toolchain with at least version 1 or higher')
+    }
+    this._builder = builder
+  }
+
+  addDefaultTasks (gulpTasks) {
+    const webpackTask = this._webpack.bind(this)
+    webpackTask.displayName = 'webpack'
+
+    gulpTasks.buildSource = series(
+      gulpTasks.buildSource,
+      webpackTask
+    )
+  }
+
+  _webpack (done) {
+    const entryGlob = (globPath, cwd) => {
+      const files = glob.sync(globPath, { cwd })
+      let entries = {}
+
+      for (let entry of files) {
+        entries[entry] = entry
+      }
+
+      return entries
+    }
+
+    return this._builder.foreachPack(
+      'webpack',
+      'behavior',
+      (pack, packDone) => {
+        const packDir = path.join(this.intermediateDir, pack.relativePath)
+        const destination = path.join(this._builder.bundleDir, pack.relativePath)
+
+        if (this.options.multiEntry) {
+          const filePaths = [].concat(
+            // glob the files
+            this.entryPoints.map((entry) => entryGlob(entry, packDir))
+          ).reduce((acc, val) => {
+            for (let key in val) {
+              // resolve the path and store by file name
+              acc[key] = path.resolve(packDir, val[key])
+            }
+            return acc
+          }, {})
+
+          return pump(
+            [
+              webpack(Object.assign({
+                // every input has one output
+                entry: filePaths,
+                output: {
+                  filename: '[name]' // the path is specified in the name
+                },
+                module: {
+                  rules: this.webpackRules
+                },
+                resolveLoader: this.options.resolveLoaders ? {
+                  // where to resolve babel-loader from... if you're adding your own you may encounter some problems here.
+                  // use the options.resolveLoaders and install babel-loader locally to get around this, or specify in webpackOptions
+                  modules: [path.join(__dirname, 'node_modules')]
+                } : {}
+              }, this.webpackOptions)),
+              dest(destination)
+            ],
+            packDone
+          )
+        } else {
+          return pump(
+            [
+              webpack(Object.assign({
+                entry: {
+                  client: path.resolve(packDir, './scripts/client/client.js'),
+                  server: path.resolve(packDir, './scripts/server/server.js')
+                },
+                output: {
+                  filename: 'scripts/[name]/[name].js'
+                },
+                module: {
+                  rules: this.webpackRules
+                },
+                resolveLoader: this.options.resolveLoaders ? {
+                  modules: [path.join(__dirname, 'node_modules')]
+                } : {}
+              }, this.webpackOptions)),
+              dest(destination)
+            ],
+            packDone
+          )
+        }
+      },
+      done
+    )
+  }
+}
+
+module.exports = WebpackSupport
